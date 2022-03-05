96067cb82d626216cd9758ddd058aa29d692145b
Pedro Jaramillo <pedro.jc.jn@gmail.com>
2022-03-05 13:58:26 -0500

    CORS y Express - primer commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config,');
+
+//Crear el servidor de express
+const app = express();
+
+// Configurar CORS
+app.use( cors() );
+
+// Base de datos
+dbConnection();
+
+// 5U3WjpoGSUuT2xaP
+// mean_user
+
+// Rutas
+app.get( '/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    })
+
+});
+
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto' + process.env.PORT );
+});
+
